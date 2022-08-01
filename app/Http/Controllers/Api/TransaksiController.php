a25ca8d86194ef09c7fe70a92b3a179b5a56cdc8
dwiwahyudi11 <wahyudydwi@gmail.com>
2022-08-01 23:55:56 +0700

    cc

--- app/Http/Controllers/Api/TransaksiController.php
+++ app/Http/Controllers/Api/TransaksiController.php
@@ -74,7 +74,7 @@ class TransaksiController extends Controller
         }
     }
 
-    public function history($id) {
+    public function history() {
         $transaksis = Transaksi::with(['user'])->whereHas('user', function ($query) use ($id) {
             $query->whereId($id);
         })->orderBy("id", "desc")->get();
